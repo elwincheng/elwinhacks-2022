d439883be12bec38905a9f535b05326cdfa912d3
Elwin Cheng <67119805+ElwinCheng@users.noreply.github.com>
2022-03-26 14:18:18 -0400

    test

--- App.tsx
+++ App.tsx
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions, Platform, StyleSheet, Text, View } from 'react-native';
+
+const {width, height} = Dimensions.get('window');
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+			<Text>YOOOO</Text>
+			<Text>hello</Text>
+			<Text>{width}</Text>
+			<Text>{height}</Text>
       <StatusBar style="auto" />
     </View>
   );
